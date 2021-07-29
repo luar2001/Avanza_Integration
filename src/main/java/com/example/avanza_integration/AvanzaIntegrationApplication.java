f517547bde98de7ef1bb3d31ca523bd92c48ca95
Lukas <Lukas.aronsson@protonmail.com>
2021-07-29 17:45:49 +0200

    Created The Project

---
+++ src/main/java/com/example/avanza_integration/AvanzaIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.example.avanza_integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvanzaIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvanzaIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/avanza_integration/AvanzaIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.avanza_integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvanzaIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
