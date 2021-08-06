4653e89e1981e916b67e7ce6f65afc1fa6ca6b98
Lukas <Lukas.aronsson@protonmail.com>
2021-08-06 18:21:27 +0200

    Changed void to appropriate return values and added TempService calls.

--- src/main/java/com/example/avanza_integration/controller/Controller.java
+++ src/main/java/com/example/avanza_integration/controller/Controller.java
@@ -1,34 +1,39 @@
 package com.example.avanza_integration.controller;
 
+import com.example.avanza_integration.services.TempService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class Controller {
 
     /**
      * BankId Login Post Mapper
+     * @return String That represents a qrCode
      */
     @PostMapping(path = "/login/bankid")
-    public void login(){
-
+    public String login(){
+       return TempService.login();
     }
 
     /**
      * BankId Status Get Mapper
+     * @return Boolean: True = LoggedIn | False = Not LoggedIn
      */
     @GetMapping(path = "/login/bankid/status")
-    public void authenticate(){
-
+    public boolean authenticate(){
+        return TempService.authenticate();
     }
 
     /**
      * Account Overview Get Mapper
+     * @return String with JSON that represents the overview.
      */
     @GetMapping(path = "/account/overview")
-    public void overview(){
-
+    public String overview(){
+        return TempService.overview();
     }
 
 }
