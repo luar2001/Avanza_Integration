792b547ce638ac194b675d32a4ea8332fb60269e
Lukas <Lukas.aronsson@protonmail.com>
2021-08-23 19:15:06 +0200

    removed the TempService.collect() call from authenticate().

--- src/main/java/com/example/avanza_integration/controller/Controller.java
+++ src/main/java/com/example/avanza_integration/controller/Controller.java
@@ -24,7 +24,6 @@ public class Controller {
      */
     @GetMapping(path = "/login/bankid/status")
     public boolean authenticate(){
-        TempService.collect();
         return TempService.authenticate();
     }
 
